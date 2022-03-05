96fe681a49f4ff83d68b61aa3ad01b13b6da9403
muntasir-i01 <you@example.communtasirm55@gmail.com>
2022-03-05 16:16:36 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const API_KEY = `efac469fa4a53e2045bf5d5b13ea7b7d`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    console.log(temperature);
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    //set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
